e8ee69e423d5483bd7223d529242d12827132127
sawan <sawankumar00121016@gmail.com>
2025-05-23 22:29:09 +0530

    Add files via upload

---
+++ Portfolio_dynamic.js
@@ -0,0 +1,9 @@
+function showsidebar(){
+    let sidbar = document.querySelector(".sidebar");
+    sidbar.style.display = 'flex';
+  }
+   function hidesidebar() {
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}
+
